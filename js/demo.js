6a791a644fe10b7fbaf8f02bb992dc1f25c56bf6
dineshkummarc <dineshkummarc@gmail.com>
2013-01-09 12:43:11 +0530

    images
    deleted images for screenshots

--- js/demo.js
+++ js/demo.js
@@ -17,10 +17,34 @@ buzz.defaults.formats = [ 'ogg', 'mp3' ];
 buzz.defaults.preload = 'metadata';
 
 var games = [
-    { img: 'img/icecream.png', color:'#7FA917', word: 'alphabets!', sound: 'sounds/truck' },
-    { img: 'img/elephant.png', color:'#BCC9CC', word: 'elephant', sound: 'sounds/elephant' },
-    { img: 'img/gorilla.png', color:'#FF0000', word: 'gorilla', sound: 'sounds/monkey' },
-    { img: 'img/lion.png', color:'#FFBE00', word: 'lion', sound: 'sounds/lion' }
+    { img: 'img/letters.png', color:'#7FA917', word: 'letters!', sound: 'sounds/truck' },
+    { img: 'img/letter_a.png', color:'#BCC9CC', word: 'a', sound: 'sounds/a' },
+	{ img: 'img/letter_b.png', color:'#BCC9CC', word: 'b', sound: 'sounds/b' },
+    { img: 'img/letter_c.png', color:'#BCC9CC', word: 'c', sound: 'sounds/c' },
+	{ img: 'img/letter_d.png', color:'#BCC9CC', word: 'd', sound: 'sounds/d' },
+	{ img: 'img/letter_e.png', color:'#BCC9CC', word: 'e', sound: 'sounds/e' },
+	{ img: 'img/letter_f.png', color:'#BCC9CC', word: 'f', sound: 'sounds/f' },
+	{ img: 'img/letter_g.png', color:'#BCC9CC', word: 'g', sound: 'sounds/g' },
+	{ img: 'img/letter_h.png', color:'#BCC9CC', word: 'h', sound: 'sounds/h' },
+	{ img: 'img/letter_i.png', color:'#BCC9CC', word: 'i', sound: 'sounds/i' },
+	{ img: 'img/letter_j.png', color:'#BCC9CC', word: 'j', sound: 'sounds/j' },
+	{ img: 'img/letter_k.png', color:'#BCC9CC', word: 'k', sound: 'sounds/k' },
+	{ img: 'img/letter_l.png', color:'#BCC9CC', word: 'l', sound: 'sounds/l' },
+	{ img: 'img/letter_m.png', color:'#BCC9CC', word: 'm', sound: 'sounds/m' },
+	{ img: 'img/letter_n.png', color:'#BCC9CC', word: 'n', sound: 'sounds/n' },
+	{ img: 'img/letter_o.png', color:'#BCC9CC', word: 'o', sound: 'sounds/o' },
+	{ img: 'img/letter_p.png', color:'#BCC9CC', word: 'p', sound: 'sounds/p' },
+	{ img: 'img/letter_q.png', color:'#BCC9CC', word: 'q', sound: 'sounds/q' },
+	{ img: 'img/letter_r.png', color:'#BCC9CC', word: 'r', sound: 'sounds/r' },
+	{ img: 'img/letter_s.png', color:'#BCC9CC', word: 's', sound: 'sounds/s' },
+	{ img: 'img/letter_t.png', color:'#BCC9CC', word: 't', sound: 'sounds/t' },
+	{ img: 'img/letter_u.png', color:'#BCC9CC', word: 'u', sound: 'sounds/u' },
+	{ img: 'img/letter_v.png', color:'#BCC9CC', word: 'v', sound: 'sounds/v' },
+	{ img: 'img/letter_w.png', color:'#BCC9CC', word: 'w', sound: 'sounds/w' },
+	{ img: 'img/letter_x.png', color:'#BCC9CC', word: 'x', sound: 'sounds/x' },
+	{ img: 'img/letter_y.png', color:'#BCC9CC', word: 'y', sound: 'sounds/y' },
+	{ img: 'img/letter_z.png', color:'#BCC9CC', word: 'z', sound: 'sounds/z' },
+    { img: 'img/end.png', color:'#FFBE00', word: '', sound: 'sounds/truck' }
 ];
 
 var winSound        = new buzz.sound('sounds/win' ),
